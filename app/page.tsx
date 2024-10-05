728efb5f16f0fea792a9f01c11ee2c878735dae5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-10-06 02:29:03 +0500

    Working ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  // const [memeData , setMemeData] = useState(null);
+  
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) =>{
+    response.json()
+    .then(data => console.log(data))
+    setMemeData(data);
+    })
+  .catch((error)=> {
+    console.log("Error ==>" , error);
+    })
+    
+    },[])    
   return (
     <>
     <div>App</div>
